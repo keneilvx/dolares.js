bff133a05e728d05c8d4cb27077caf68d70870d1
Keneil Smith <66431476+keneilvx@users.noreply.github.com>
2025-08-18 22:24:34 +0100

    removed unnecessary files

--- main.js
+++
@@ -1,8 +0,0 @@
-let dollar = dolares("YEN", "", 100)
-
-
-// console.log(a.Currency());
-// console.log(a.check_currency())
-// console.log(a.available_currencies())
-console.log(dollar.Currency().format(true))
-// console.log(dollar.appendCode())
